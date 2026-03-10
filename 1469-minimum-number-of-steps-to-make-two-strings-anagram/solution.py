0541ed9a9e211effa0d042ff6ab84c4b05f46f54
Asher Jamshaid <asherjamshaid112@gmail.com>
2026-03-10 11:41:08 +0000

    Sync LeetCode submission Runtime - 187 ms (48.78%), Memory - 14.4 MB (6.10%)

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minSteps(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        """
+        hashing = [0] * 26
+        hashing2 = [0] * 26
+        ans = 0
+        for x in range(len(s)):
+            hashing[ord(s[x]) - ord("a")]+=1
+        for x in range(len(s)):
+            hashing2[ord(t[x]) - ord("a")]+=1
+        for x in range(26):
+            if hashing[x] > hashing2[x]:
+                ans+=(hashing[x]-hashing2[x])
+        return ans
+            
