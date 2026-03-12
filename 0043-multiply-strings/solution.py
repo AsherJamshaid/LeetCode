4c95ee9e7f7ec32e2ee179bb05f5b90e8f7f5016
Asher Jamshaid <asherjamshaid112@gmail.com>
2026-03-12 12:28:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (58.88%)

---
+++ 0043-multiply-strings/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        one = int(num1)
+        two = int(num2)
+        res = one * two
+        return str(res)
