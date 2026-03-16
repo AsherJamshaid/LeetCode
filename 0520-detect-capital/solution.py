0e81e028072b1efd13043404bfee2884dd353f8c
Asher Jamshaid <asherjamshaid112@gmail.com>
2026-03-16 14:41:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (0.47%)

---
+++ 0520-detect-capital/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        
+        if word.isupper():
+            return True
+        if word.islower():
+            return True
+        if word[0].isupper():
+            if word[1::].islower():
+                return True
+        return False                    
