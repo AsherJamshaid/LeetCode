0c6b91564037f9d83a1ba23fdc3f3bdbb3806858
Asher Jamshaid <asherjamshaid112@gmail.com>
2026-03-17 19:50:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.1 MB (44.23%)

---
+++ 0414-third-maximum-number/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        unique_nums = list(set(nums)) 
+        if len(unique_nums) < 3:
+            return max(unique_nums)
+        unique_nums.sort(reverse=True)
+        return unique_nums[2]
