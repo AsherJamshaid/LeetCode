e4c80a8bd2a41aaec027aae58167eca52cf301fa
Asher Jamshaid <asherjamshaid112@gmail.com>
2026-03-19 12:07:39 +0000

    Sync LeetCode submission Runtime - 4 ms (49.83%), Memory - 15.2 MB (9.15%)

---
+++ 0137-single-number-ii/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hashing = {}
+
+        for x in range(len(nums)):
+            if nums[x] in hashing:
+                hashing[nums[x]]+=1
+            else:
+                hashing[nums[x]] = 1
+        
+        for key,val in hashing.items():
+            if val == 1:
+                return key
