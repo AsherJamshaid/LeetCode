fa286e588f1bdea3c06375cf39f4aa9197eb8ecc
Asher Jamshaid <asherjamshaid112@gmail.com>
2026-04-19 07:01:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (19.30%)

---
+++ 1289-day-of-the-week/solution.py
@@ -0,0 +1,11 @@
+import datetime
+class Solution(object):
+    def dayOfTheWeek(self, day, month, year):
+        """
+        :type day: int
+        :type month: int
+        :type year: int
+        :rtype: str
+        """
+        d = datetime.date(year, month, day)
+        return(d.strftime("%A"))  
