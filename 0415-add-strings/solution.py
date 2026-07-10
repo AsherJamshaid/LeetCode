00cd9e9f3dd7ec01e168884fc246660b3f2086e9
Asher Jamshaid <asherjamshaid112@gmail.com>
2026-07-10 12:04:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (61.31%)

---
+++ 0415-add-strings/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        int1 = int(num1)
+        int2 = int(num2)
+        return str(int1 + int2)
