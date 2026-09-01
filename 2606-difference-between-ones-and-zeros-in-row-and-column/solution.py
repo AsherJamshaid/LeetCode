57f279e785e5bd38208eb47c5f1433e6553f41df
Asher Jamshaid <asherjamshaid112@gmail.com>
2026-09-01 15:52:09 +0000

    Sync LeetCode submission Runtime - 222 ms (48.87%), Memory - 56.8 MB (85.71%)

---
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/solution.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def onesMinusZeros(self, grid):
+
+        m = len(grid)
+        n = len(grid[0])
+
+        onesRow = [0] * m
+        zerosRow = [0] * m
+        onesCol = [0] * n
+        zerosCol = [0] * n
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    onesRow[i] += 1
+                    onesCol[j] += 1
+                else:
+                    zerosRow[i] += 1
+                    zerosCol[j] += 1
+
+        diff = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                diff[i][j] = onesRow[i] + onesCol[j] - zerosRow[i] - zerosCol[j]
+
+        return diff
